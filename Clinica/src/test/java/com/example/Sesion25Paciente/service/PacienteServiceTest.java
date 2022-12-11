b8ceb82a8fa9a47be4abe726ef70f003205634ef
Catalina Hernandez <cataxto@gmail.com>
2022-12-10 22:28:00 -0500

    Adicionando los Test sobre Paciente Service

---
+++ Clinica/src/test/java/com/example/Sesion25Paciente/service/PacienteServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.Sesion25Paciente.service; //package com.example.Sesion25Paciente
+
+import com.example.Sesion25Paciente.dto.PacienteDto;
+import com.example.Sesion25Paciente.exception.ResourceNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PacienteServiceTest {
+
+    private PacienteService pacienteService;
+
+    @Test
+    public void testCrearPaciente() {
+        PacienteDto pacienteDton1 = new PacienteDto("Juan","Hernandez","Calle 20 21","1017",new Date());
+        pacienteService.guardar(pacienteDton1);
+        Optional<PacienteDto> pacienteDto1 = pacienteService.buscar(1);
+        assertTrue(pacienteDto1.isPresent());
+    }
+
+    @Test
+    public void testListaPacientes(){
+        PacienteDto pacienteDton1 = new PacienteDto("Juan","Hernandez","Calle 20 21","1017",new Date());
+        pacienteService.guardar(pacienteDton1);
+        PacienteDto pacienteDton2 = new PacienteDto("Carlos","Montero","Calle 20 22","1018",new Date());
+        pacienteService.guardar(pacienteDton2);
+        PacienteDto pacienteDton3 = new PacienteDto("Benny","Arbelaez","Calle 20 23","1019",new Date());
+        pacienteService.guardar(pacienteDton3);
+        Set<PacienteDto> pacientes = pacienteService.listar();
+        assertEquals(3, pacientes.size());
+    }
+
+    @Test
+    public void testActualizarPaciente() throws ResourceNotFoundException {
+        PacienteDto pacienteDtonActual = new PacienteDto("Juan","Hernandez","Calle 20 21","1017",new Date());
+        pacienteService.guardar(pacienteDtonActual); //guardar paciente 1 Original
+        PacienteDto pacienteDtonNuevo = new PacienteDto("Juan","Perez","Calle 20 21","1017",new Date());
+        pacienteService.actualizar(1,pacienteDtonNuevo);
+        Optional<PacienteDto> pacienteDtoActualizado = pacienteService.buscar(1);
+        assertEquals(pacienteDtoActualizado.get().apellido,pacienteDtonNuevo.apellido);
+    }
+
+    @Test
+    public void testEliminarPaciente() throws ResourceNotFoundException {
+        PacienteDto pacienteDton1 = new PacienteDto("Juan","Hernandez","Calle 20 21","1017",new Date());
+        pacienteService.guardar(pacienteDton1);
+        pacienteService.eliminar(1);
+        Optional<PacienteDto> pacienteDto1 = pacienteService.buscar(1);
+        assertFalse(pacienteDto1.isPresent());
+    }
+}
